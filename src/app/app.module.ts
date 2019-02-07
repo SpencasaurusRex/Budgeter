7f97f70505ee40facc6ed50f69d7a0f85ecf245a
Spencer Haney <SpencasaurusRex@gmail.com>
2019-02-07 01:56:24 -0500

    Implemented part of angular tutorial and started conversion to apply it to app

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SourcesComponent } from './sources/sources.component';
+import { ClassifyComponent } from './classify/classify.component';
+import { RulesComponent } from './rules/rules.component';
+import { BreakdownComponent } from './breakdown/breakdown.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    SourcesComponent,
+    ClassifyComponent,
+    RulesComponent,
+    BreakdownComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
